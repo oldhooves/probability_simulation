54f34ec32de0d5da7711eb03a20005bf4ed6dd33
oldhooves <465304942@qq.com>
2018-05-24 16:50:20 +0800

    扫雷floodfill算法

--- 06Mine-Sweeper/02-Mine-Sweeper-Basics/src/MineSweeperData.java
+++ 06Mine-Sweeper/02-Mine-Sweeper-Basics/src/MineSweeperData.java
@@ -11,6 +11,9 @@ public class MineSweeperData {
 
     private int N, M;
     private boolean[][] mines;
+    private int[][] numbers;
+    public boolean[][] open;
+    public boolean[][] flags;
 
     public MineSweeperData(int N, int M, int mineNumber){
 
@@ -24,12 +27,19 @@ public class MineSweeperData {
         this.M = M;
 
         mines = new boolean[N][M];
+        numbers = new int[N][M];
+        open = new boolean[N][M];
+        flags = new boolean[N][M];
         for(int i = 0 ; i < N ; i ++)
             for(int j = 0 ; j < M ; j ++){
                 mines[i][j] = false;
+                numbers[i][j] = 0;
+                open[i][j] = false;
+                flags[i][j] = false;
             }
 
         generateMines(mineNumber);
+        calculateNumbers();
     }
 
     public int N(){ return N; }
@@ -41,34 +51,81 @@ public class MineSweeperData {
         return mines[x][y];
     }
 
+    public int getNumber(int x, int y){
+        if(!inArea(x, y))
+            throw new IllegalArgumentException("Out of index in getNumber function!");
+        return numbers[x][y];
+    }
+
     public boolean inArea(int x, int y){
         return x >= 0 && x < N && y >= 0 && y < M;
     }
 
     private void generateMines(int mineNumber){
 
-        for (int i = 0; i < mineNumber; i++) {
-            int x = i / M;
-            int y = i % M;
+        for(int i = 0 ; i < mineNumber ; i ++){
+            int x = i/M;
+            int y = i%M;
             mines[x][y] = true;
         }
 
-        int swapTime = N*M;
-        for (int i = 0; i < swapTime; i++) {
-            int x1 = (int)(Math.random() * N);
-            int y1 = (int)(Math.random() * M);
+        for(int i = N*M-1; i >= 0 ; i-- ){
 
-            int x2 = (int)(Math.random() * N);
-            int y2 = (int)(Math.random() * M);
+            int iX = i/M;
+            int iY = i%M;
 
-            swap(x1,y1,x2,y2);
+            int randNumber = (int)(Math.random()*(i+1));
+
+            int randX = randNumber/M;
+            int randY = randNumber%M;
+
+            swap(iX, iY, randX, randY);
         }
     }
 
-    public void swap(int x1,int y1,int x2,int y2){
+    private void swap(int x1, int y1, int x2, int y2){
         boolean t = mines[x1][y1];
         mines[x1][y1] = mines[x2][y2];
         mines[x2][y2] = t;
     }
 
+    private void calculateNumbers(){
+
+        for(int i = 0 ; i < N ; i ++)
+            for(int j = 0 ; j < M ; j ++){
+
+                if(mines[i][j])
+                    numbers[i][j] = -1;
+
+                numbers[i][j] = 0;
+                for(int ii = i-1 ; ii <= i+1 ; ii ++)
+                    for(int jj = j-1 ; jj <= j+1 ; jj ++)
+                        if(inArea(ii, jj) && isMine(ii, jj))
+                            numbers[i][j] ++;
+            }
+
+        return;
+    }
+
+    public void open(int x,int y){
+        if (!inArea(x,y)){
+            throw new IllegalArgumentException("越界");
+        }
+        if (isMine(x, y)){
+            throw new IllegalArgumentException("不应该有雷");
+        }
+        open[x][y] = true;
+
+        if (numbers[x][y] >0){
+            return;
+        }
+        for (int i = x-1; i <= x + 1 ; i++) {
+            for (int j = y-1;j <=y + 1;j++){
+                if (inArea(i,j) && !open[i][j] && !mines[i][j]){
+                    open(i,j);
+                }
+            }
+            
+        }
+    }
 }
